50c394952cad2e40d450ef305021be5596fdfc48
Moez <moez.benjemiaa@etudiant-fst.utm.tn>
2025-10-20 11:33:41 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route, useParams, useLocation } from "react-router-dom";
+
+import './App.css';
+import LoginPage from "./Login/YetiLogin";
+import NotFoundPage from "./404/404";
+
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
